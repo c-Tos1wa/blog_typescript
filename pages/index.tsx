0e34a766707f35777bc03e6def2b5b9ca097e619
c-Tos1wa <cristoshiwassaki@gmail.com>
2022-08-27 16:29:38 -0300

    about me introduction and adjusts text style

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,29 @@ export default function Home({ posts }: {
       <NavBar />
       <section className={styles.text}>
         <p>
-           
+          Formou-se em Física Médica pela Universidade Estadual Paulista "Julio de Mesquita Filho" (Unesp)
+          campus de Botucatu, onde entrou em contato com algoritmo, lógica de programação.
+        </p>
+        <p>
+          Após a faculdade, estudou para concursos públicos, sem muito sucesso, mas, por curiosidade, se manteve
+          atualizada nas novas tecnologias.
+          Achou interessante como os dados poderiam ser utilizados para criar padrões, detectar anomalias e predizer resultados
+          por isso, se interessou em ciência de dados.
+        </p>
+        <p>
+          Durante a pandemia, começou estudando Python, mas foi redirecionada ao desenvolvimento web, mais precisamente
+          ao Fullstack em Node.
+        </p>
+        <p>
+          Conhece, no back-end, Express, Nest.Js e as ORM Prisma e Sequelize, além de MySQL e Postgres como 
+          banco de dados. No front-end, Next.Js e básico de Angular.Js, além de frameworks, como Bootstrap, Material-UI e Chakra-UI.
+        </p>
+        <p>
+          Para se manter atualizada, continua estudando as bibliotecas e frameworks do Javascript e em paralelo,
+          tenta criar portfólio relacionado a dados, estatística e ciência de dados.
+        </p>
+        <p>
+          Este blog será direcionado aos projetos de estudo publicados no Github.
         </p>
       </section>
       <section>
